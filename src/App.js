82aa62f1c970a9735d4f55fe0930fee4da2f93eb
Alija Fajić <62569376+alija98@users.noreply.github.com>
2021-06-10 15:28:01 +0200

    Updating components

--- src/App.js
+++ src/App.js
@@ -9,45 +9,39 @@ const api = {
 };
 
 function App() {
-  const [loading, setLoading] = useState(false);
   const [cityName, setCityName] = useState("Mostar");
-  const [weatherToday, setWeatherToday] = useState({});
-  const [weatherForecast, setWeatherForecast] = useState({});
+  const [weatherToday, setWeatherToday] = useState({
+    name: "",
+    main: {},
+    weather: [{}],
+  });
+
+  const [weatherForecast, setWeatherForecast] = useState({
+    city: {},
+  });
 
   function get5DayData() {
-    setLoading(true);
-    const response = fetch(
-      `${api.base_5_day}${cityName}&units=metric&APPID=${api.key}`
-    );
-    const data_5_day = response.json();
-    if (data_5_day) {
-      setWeatherForecast(data_5_day);
-    }
-    setLoading(false);
+    fetch(`${api.base_5_day}${cityName}&units=metric&APPID=${api.key}`)
+      .then((response) => response.json())
+      .then((data) => setWeatherForecast(data));
   }
 
   function getTodayData() {
-    setLoading(true);
-    const response = fetch(
-      `${api.base_today}${cityName}&units=metric&APPID=${api.key}`
-    );
-    const data_today = response.json();
-    if (data_today) {
-      setWeatherToday(data_today);
-    }
-    setLoading(false);
+    fetch(`${api.base_today}${cityName}&units=metric&APPID=${api.key}`)
+      .then((response) => response.json())
+      .then((data) => setWeatherToday(data));
   }
 
   useEffect(() => {
     get5DayData();
     getTodayData();
   }, []);
-  if (!weatherToday) {
-    return null;
-  }
+
   return (
     <section className="main__section">
-      <Box weatherToday={weatherToday ? weatherToday : ""} loading={loading} />
+      <Box weatherToday={weatherToday} />
+      <Box weatherForecast={weatherForecast} />
+      <span></span>
     </section>
   );
 }
--- src/Box.js
+++ src/Box.js
@@ -1,68 +1,44 @@
 import React, { useState, useRef } from "react";
 
-const Box = ({ weatherToday, weatherForecast, loading }) => {
-  console.log(weatherToday);
-
-  if (!weatherToday) {
-    return null;
-  }
-  if (loading) {
-    return <div className="box">Loading...</div>;
-  } else {
+const Box = ({ weatherToday, weatherForecast }) => {
+  console.log(weatherForecast);
+  if (weatherToday) {
     return (
       <div className="box">
-        <div className="box">
-          <div className="box__heading">
-            <h3>Monday</h3>
-            <h3>1 June</h3>
-          </div>
-          <div className="box__body">
-            {typeof weatherToday != "undefined" ? (
-              <h3>{weatherToday.name}</h3>
-            ) : (
-              ""
-            )}
-            {typeof weatherToday != "undefined" ? (
-              <h2>{Math.round(weatherToday.main.temp)}</h2>
-            ) : (
-              ""
-            )}
-
-            <div className="weather__data"></div>
+        <div className="box__heading">
+          <h3>Monday</h3>
+          <h3>1 June</h3>
+        </div>
+        <div className="box__body">
+          <h3>{weatherToday.name}</h3>
+          <h2>{Math.round(weatherToday.main.temp)}</h2>
+          <h2>ICON</h2>
+          <div className="weather__data">
+            <span>{weatherToday.weather[0].description}</span>
           </div>
         </div>
       </div>
-
-      /*
-    ----- 
-----   <h2>{Math.round(weatherToday.main.temp)}</h2>
-
-
-            {typeof weatherToday != "undefined" ? (
-              <div className="box">
-           
-  <div className="box__heading">
-                <h3>Monday</h3>
-                <h3>3 May</h3>
-              </div>
-              <div className="box__body">
-                <h3>{weatherToday.cityName}</h3>
-                <div className="weather__data">
-              <h2>{Math.round(weatherToday.main.temp)}</h2>
-              <span>{weatherToday.weather[0].description}</span>
-              </div>
-              </div>
-              <div className="box__footer">
-                <span>I</span>
-                <span>I</span>
-                <span>I</span>
-              </div>
-            
-            </div>
-            ) : (
-              "sa"
-            )}
-          */
+    );
+  } else {
+    return (
+      <div className="box">
+        <div className="box__heading">
+          <h3>Monday</h3>
+          <h3>1 June</h3>
+        </div>
+        <div className="box__body">
+          <h3>{weatherForecast.city.name}</h3>
+          {
+            //<h3>{weatherForecast.list[7].main.temp}</h3>
+          }
+          <h2>ICON</h2>
+          <div className="weather__data">
+            {
+              //<span>{weatherForecast.list[7].weather[0].description}</span>
+            }
+          </div>
+        </div>
+      </div>
     );
   }
 };
