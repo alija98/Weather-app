f803c887616f802099c1d1bc9ae662ee399387c2
Alija Fajić <62569376+alija98@users.noreply.github.com>
2021-06-14 14:27:04 +0200

    Updating components

--- src/App.js
+++ src/App.js
@@ -3,45 +3,155 @@ import "./App.css";
 import Box from "./Box";
 
 const api = {
-  key: "882c9ebd1015abd13a008e463715353e",
+  key: "6ca0b498f0fec56b7af4fd7f50c36eac",
   base_5_day: "https://api.openweathermap.org/data/2.5/forecast?q=",
   base_today: "https://api.openweathermap.org/data/2.5/weather?q=",
 };
 
+const dayBuilder = (d) => {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[d.getDay()];
+  return `${day}`;
+};
+
+const dateBuilder = (d) => {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+
+  return ` ${date} ${month}`;
+};
+//seting today, tomorrow and aftertomorrow dates
+const today = new Date();
+const tomorrow = new Date(today);
+tomorrow.setDate(tomorrow.getDate() + 1);
+const afterTomorrow = new Date(today);
+afterTomorrow.setDate(afterTomorrow.getDate() + 2);
+
 function App() {
-  const [cityName, setCityName] = useState("Mostar");
-  const [weatherToday, setWeatherToday] = useState({
-    name: "",
-    main: {},
-    weather: [{}],
-  });
-
-  const [weatherForecast, setWeatherForecast] = useState({
-    city: {},
-  });
-
+  const [searchTerm, setSearchTerm] = useState("Mostar");
+  const [cityName, setCityName] = useState("");
+  const [temperatureToday, setTemperatureToday] = useState("");
+  const [temperatureTomorrow, setTemperatureTomorrow] = useState("");
+  const [temperatureAfterTomorrow, setTemperatureAfterTomorrow] = useState("");
+  const [descriptionToday, setDescriptionToday] = useState("");
+  const [descriptionTomorrow, setDescriptionTomorrow] = useState("");
+  const [descriptionAfterTomorrow, setDescriptionAfterTomorrow] = useState("");
+  const [imageToday, setImageToday] = useState("");
+  const [imageTomorrow, setImageTomorrow] = useState("");
+  const [imageAfterTomorrow, setImageAfterTomorrow] = useState("");
+  //fetching data for tomorrow and after tomorrov
   function get5DayData() {
-    fetch(`${api.base_5_day}${cityName}&units=metric&APPID=${api.key}`)
+    const search = searchTerm ? searchTerm : "a";
+    fetch(`${api.base_5_day}${search}&units=metric&APPID=${api.key}`)
       .then((response) => response.json())
-      .then((data) => setWeatherForecast(data));
+      .then((data) => {
+        if (data.cod !== "404") {
+          setCityName(data.city.name);
+          setTemperatureTomorrow(Math.round(data.list[7].main.temp));
+          setTemperatureAfterTomorrow(Math.round(data.list[15].main.temp));
+          setDescriptionTomorrow(data.list[7].weather[0].description);
+          setDescriptionAfterTomorrow(data.list[15].weather[0].description);
+          setImageTomorrow(data.list[7].weather[0].icon);
+          setImageAfterTomorrow(data.list[15].weather[0].icon);
+        } else {
+        }
+      });
   }
-
+  //data for today
   function getTodayData() {
-    fetch(`${api.base_today}${cityName}&units=metric&APPID=${api.key}`)
+    const search = searchTerm ? searchTerm : "a";
+    fetch(`${api.base_today}${search}&units=metric&APPID=${api.key}`)
       .then((response) => response.json())
-      .then((data) => setWeatherToday(data));
-  }
+      .then((data) => {
+        console.log(data);
 
-  useEffect(() => {
+        if (data.cod !== "404") {
+          setCityName(data.name);
+          setTemperatureToday(Math.round(data.main.temp));
+          setDescriptionToday(data.weather[0].description);
+          setImageToday(data.weather[0].icon);
+        } else {
+        }
+      });
+  }
+  const FetchData = () => {
     get5DayData();
     getTodayData();
+  };
+
+  useEffect(() => {
+    FetchData();
   }, []);
 
   return (
-    <section className="main__section">
-      <Box weatherToday={weatherToday} />
-      <Box weatherForecast={weatherForecast} />
-      <span></span>
+    <section className="app">
+      <div className="boxes">
+        <Box
+          cityName={cityName}
+          temperatureToday={temperatureToday}
+          descriptionToday={descriptionToday}
+          imageToday={imageToday}
+          day={dayBuilder(today)}
+          date={dateBuilder(today)}
+        />
+        <Box
+          cityName={cityName}
+          temperatureTomorrow={temperatureTomorrow}
+          descriptionTomorrow={descriptionTomorrow}
+          imageTomorrow={imageTomorrow}
+          day={dayBuilder(tomorrow)}
+          date={dateBuilder(tomorrow)}
+        />
+        <Box
+          cityName={cityName}
+          temperatureAfterTomorrow={temperatureAfterTomorrow}
+          descriptionAfterTomorrow={descriptionAfterTomorrow}
+          imageAfterTomorrow={imageAfterTomorrow}
+          day={dayBuilder(afterTomorrow)}
+          date={dateBuilder(afterTomorrow)}
+        />
+      </div>
+      <form onSubmit={() => FetchData()}>
+        <input
+          className="input"
+          placeholder="Mostar"
+          value={searchTerm}
+          onChange={(e) => {
+            setSearchTerm(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            FetchData();
+          }}
+        >
+          Search the City
+        </button>
+      </form>
     </section>
   );
 }
--- src/Box.js
+++ src/Box.js
@@ -1,46 +1,44 @@
 import React, { useState, useRef } from "react";
 
-const Box = ({ weatherToday, weatherForecast }) => {
-  console.log(weatherForecast);
-  if (weatherToday) {
-    return (
-      <div className="box">
-        <div className="box__heading">
-          <h3>Monday</h3>
-          <h3>1 June</h3>
-        </div>
-        <div className="box__body">
-          <h3>{weatherToday.name}</h3>
-          <h2>{Math.round(weatherToday.main.temp)}</h2>
-          <h2>ICON</h2>
-          <div className="weather__data">
-            <span>{weatherToday.weather[0].description}</span>
-          </div>
-        </div>
+const Box = ({
+  cityName,
+  temperatureToday,
+  descriptionToday,
+  temperatureTomorrow,
+  temperatureAfterTomorrow,
+  descriptionTomorrow,
+  descriptionAfterTomorrow,
+  imageToday,
+  imageTomorrow,
+  imageAfterTomorrow,
+  day,
+  date,
+}) => {
+  const temperature =
+    temperatureToday || temperatureTomorrow || temperatureAfterTomorrow;
+  const description =
+    descriptionToday || descriptionTomorrow || descriptionAfterTomorrow;
+  const image = imageToday || imageTomorrow || imageAfterTomorrow;
+
+  return (
+    <div className="box">
+      <div className="box__heading">
+        <h5>{day}</h5>
+        <h5>{date}</h5>
       </div>
-    );
-  } else {
-    return (
-      <div className="box">
-        <div className="box__heading">
-          <h3>Monday</h3>
-          <h3>1 June</h3>
-        </div>
-        <div className="box__body">
-          <h3>{weatherForecast.city.name}</h3>
-          {
-            //<h3>{weatherForecast.list[7].main.temp}</h3>
-          }
-          <h2>ICON</h2>
-          <div className="weather__data">
-            {
-              //<span>{weatherForecast.list[7].weather[0].description}</span>
-            }
-          </div>
-        </div>
+      <h2 className="city__name">{cityName}</h2>
+      <div className="box__body">
+        <h2>{temperature}°C</h2>
+        <img
+          src={`http://openweathermap.org/img/wn/${image}@2x.png`}
+          alt="weather"
+        />
       </div>
-    );
-  }
+      <div className="weather__data">
+        <span>{description}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Box;
